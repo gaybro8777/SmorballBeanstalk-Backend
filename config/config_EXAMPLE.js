3dac148110f2c93e43d9a49cb862865492ed0402
Gareth Solbeck <gsolbeck@gmail.com>
2015-06-24 10:54:35 -0400

    Create config file template

--- config/config_EXAMPLE.js
+++ config/config_EXAMPLE.js
@@ -1,7 +1,15 @@
+/**
+ * This is an example configuration file. To set up your own, copy this file to
+ *  "config.js" and substitute your secret into the marked line below.
+ *
+ * Backend server for Smorball & Beanstalk
+ * Tiltfactor, 2015
+ */
+
 module.exports = {
     productionDB: 'mongodb://localhost/BHLMaster',
     developmentDB: 'mongodb://localhost/BHLTesting',
-    secret: 'zsZ,OgPp{W>wj:;`UB^Y3xh&Eg;e1:VTsrCv[%6*Y)Z>YS`bFSUmRvgDwEm!FOhe',
+    secret: /* enter secret here when creating config file */,
     ServerPort: 8081,
     /* The thresholds control when differences are marked as "tagged" or "passed"
      *  passes / (sum of weights + passes) > passedRatio
